6adfc08394e6696654fe5d9726eb78fe7352cca4
Arhaan Vasim Shaikh <shkarhaan205@gmail.com>
2025-08-15 00:05:53 +0530

    Initial commit for search.java

---
+++ stack parenthesis.java
@@ -0,0 +1,65 @@
+// import java.util.*;
+// class Solution {
+
+//     public static boolean isValid(String s) {
+//          Stack<Character> stack = new Stack<>();
+
+//         for (char ch : s.toCharArray()) {
+//             if (ch == '(' || ch == '{' || ch == '[') {
+//                 stack.push(ch);
+//             } else {
+//                 if (stack.isEmpty()) return false;
+
+//                 char top = stack.pop();
+//                 if ((ch == ')' && top != '(') ||
+//                     (ch == '}' && top != '{') ||
+//                     (ch == ']' && top != '[')) {
+//                     return false;
+//                 }
+//             }
+//         }
+
+//         return stack.isEmpty();
+//     }
+
+//     public static void main(String[] args) {
+//         System.out.println(isValid("()"));       // true
+//         System.out.println(isValid("()[]{}"));   // true
+//         System.out.println(isValid("(]"));       // false
+//     }
+// }
+
+
+class Solution {
+
+public static boolean isValid(String s){
+    Stack<Character> stack = new Stack<>();
+
+    for (char ch : s.toCharArray()) {
+        if (ch == '(' || ch == '{' || ch == '[') {
+            stack.push(ch);
+        } else {
+            if (stack.isEmpty()) return false;
+
+            char top = stack.pop();
+            if ((ch == ')' && top != '(') ||
+                     (ch == '}' && top != '{') ||
+                     (ch == ']' && top != '[')) {
+                     return false;
+             
+        } 
+
+    
+        }
+}
+return stack.isEmpty();
+
+}
+      public static void main(String[] args) {
+         System.out.println(isValid("()"));       // true
+         System.out.println(isValid("()[]{}"));   // true
+                System.out.println(isValid("(]"));       // false
+}
+
+
+ }
